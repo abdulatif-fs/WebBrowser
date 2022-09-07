d681cc680ad42577af55d8a4d6bf61533c7ff82c
abdulatif-fs <fasijardiq@gmail.com>
2022-09-08 01:33:36 +0700

    clickable link masih berupa button

--- browser.py
+++ browser.py
@@ -3,6 +3,12 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
 
+class hyperlink(QLabel):
+    def __init__(self, parent:None):
+        super().__init__()
+        self.setOpenExternalLinks(True)
+        self.setParent(parent)
+
 class WebBrowser(QMainWindow):
     def __init__(self):
         super(WebBrowser, self).__init__()
@@ -24,17 +30,24 @@ class WebBrowser(QMainWindow):
 
         self.forward_btn = QPushButton(">")
         self.forward_btn.setMinimumHeight(30)
+        
+        #self.youtube = QLabel('youtube')
+        #self.youtube.setMinimumHeight(30)
+        self.youtube = QPushButton('youtube')
 
         self.horizontal.addWidget(self.url_bar)
         self.horizontal.addWidget(self.go_btn)
         self.horizontal.addWidget(self.back_btn)
         self.horizontal.addWidget(self.forward_btn)
+        self.horizontal.addWidget(self.youtube)
 
         self.browser = QWebEngineView()
 
         self.go_btn.clicked.connect(lambda: self.navigate(self.url_bar.toPlainText()))
         self.back_btn.clicked.connect(self.browser.back)
         self.forward_btn.clicked.connect(self.browser.forward)
+        #self.youtube.
+        self.youtube.clicked.connect(lambda: self.browser.setUrl(QUrl("http://youtube.com")))
 
         self.layout.addLayout(self.horizontal)
         self.layout.addWidget(self.browser)
